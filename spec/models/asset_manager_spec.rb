d44ba09d0550e55a7b4d2dfcc832cb85dcb64429
Marty Haught <mghaught@gmail.com>
2008-05-13 15:07:09 -0600

    Added more AssetManager specs

--- spec/models/asset_manager_spec.rb
+++ spec/models/asset_manager_spec.rb
@@ -17,51 +17,153 @@ describe Yui4Rails::AssetManager, ".new" do
 
 end
 
-# 
-# it "should return appropriate resources for :container" do
-#  	@caller.include_yui(:container)
-#  	@caller.yui_includes.should == "/yui/container/assets/container\n/yui/yahoo-dom-event/yahoo-dom-event\n/yui/animation/animation-min\n/yui/container/container-min"
-# end
-# 
-# it "should return appropriate resources for :datatable" do
-#  	@caller.include_yui(:datatable)
-#  	@caller.yui_includes.should == "/yui/datatable/assets/skins/sam/datatable\n/yui/utilities/utilities\n/yui/datasource/datasource-beta-min\n/yui/datatable/datatable-beta-min"
-# end
-# 
-# it "should return appropriate resources for :charts" do
-#  	@caller.include_yui(:charts)
-#  	@caller.yui_includes.should == "/yui/utilities/utilities\n/yui/datasource/datasource-beta-min\n/yui/json/json-min\n/yui/charts/charts-experimental-min\n<script type=\"text/javascript\" charset=\"utf-8\">\nYAHOO.widget.Chart.SWFURL = \"/yui/charts/assets/charts.swf\";\n</script>"
-# end
-# 
-# it "should return appropriate resources for :carousel" do
-#  	@caller.include_yui(:carousel)
-#  	@caller.yui_includes.should == "/yui/carousel/assets/carousel\n/yui/yahoo-dom-event/yahoo-dom-event\n/yui/animation/animation-min\n/yui/container/container-min\n/yui/carousel/carousel_min"
-# end
-# describe Yui4Rails::IncludeExtension, ".include_yui" do
-# 	
-# 	before(:all) do
-# 	  @caller = AppMock.new
-# 	end
-# 	
-#   it "should initialize @yui_components as an empty array" do
-#     @caller.include_yui
-# 		@caller.yui_components.should an_instance_of(Array)
-# 		@caller.yui_components.should be_empty
-#   end	
-# 
-#   it "should add one component to @yui_components" do
-#     @caller.include_yui(:charts)
-# 		@caller.yui_components.should an_instance_of(Array)
-# 		@caller.yui_components.should_not be_empty
-# 		@caller.yui_components.should include(:charts)
-#   end
-# 
-# 	it "should allow duplicate entries and still record at least one entry" do
-# 	  @caller.include_yui(:charts)
-# 	  @caller.include_yui(:charts, :datatable)
-# 		@caller.yui_components.should an_instance_of(Array)
-# 		@caller.yui_components.should_not be_empty
-# 		@caller.yui_components.should include(:charts) 
-# 		@caller.yui_components.should include(:datatable) 
-# 	end
-# end
+describe Yui4Rails::AssetManager, ".reset" do
+  it "should nil the @@manager" do
+		Yui4Rails::AssetManager.class_eval("@@manager = 123")
+		Yui4Rails::AssetManager.class_eval("@@manager").should == 123
+		Yui4Rails::AssetManager.reset
+		Yui4Rails::AssetManager.class_eval("@@manager").should be_nil
+  end
+end
+
+
+describe Yui4Rails::AssetManager, ".manager" do
+  it "should call .new if manager is nil" do
+		Yui4Rails::AssetManager.should_receive(:new)
+		Yui4Rails::AssetManager.class_eval("@@manager = nil")
+		Yui4Rails::AssetManager.class_eval("@@manager").should be_nil
+		Yui4Rails::AssetManager.manager
+  end
+
+	it "should set @@manager" do
+		@manager = Yui4Rails::AssetManager.manager
+		Yui4Rails::AssetManager.class_eval("@@manager").should == @manager
+  end
+
+	it "should return an instance of AssetManager" do
+	  Yui4Rails::AssetManager.manager.should be_an_instance_of(Yui4Rails::AssetManager)
+	end
+end
+
+describe Yui4Rails::AssetManager, ".add_script" do
+  it "should add a string arg to the @scripts string" do
+		@manager = Yui4Rails::AssetManager.new
+		@manager.scripts.should == ""
+		@manager.add_script("my script")
+		@manager.scripts.should == "my script"
+  end
+end
+
+describe Yui4Rails::AssetManager, ".add_components" do
+  it "should add a string arg to the @scripts string" do
+		@manager = Yui4Rails::AssetManager.new
+		@manager.scripts.should == ""
+		@manager.add_script("my script")
+		@manager.scripts.should == "my script"
+  end
+end
+
+describe Yui4Rails::AssetManager, ".stylesheets" do
+	
+	before(:each) do
+	  @manager = Yui4Rails::AssetManager.new
+		@stylesheets = ["some","stuff"]
+	end
+	
+  it "should call process_components if @stylesheets is nil" do
+		@manager.instance_variable_get(:@stylesheets).should be_nil
+		@manager.should_receive(:process_components)
+		@manager.stylesheets
+  end
+
+ 	it "should not call process_components if @stylesheets exists" do
+		@manager.instance_variable_set(:@stylesheets, @stylesheets) 
+		@manager.should_not_receive(:process_components)
+		@manager.stylesheets
+  end
+
+	it "should return @stylesheets" do
+		@manager.instance_variable_set(:@stylesheets, @stylesheets)
+		@manager.stylesheets.should == @stylesheets
+	end
+end
+
+
+describe Yui4Rails::AssetManager, ".javascripts" do
+	
+	before(:each) do
+	  @manager = Yui4Rails::AssetManager.new
+		@javascripts = ["some","stuff"]
+	end
+	
+  it "should call process_components if @javascripts is nil" do
+		@manager.instance_variable_get(:@javascripts).should be_nil
+		@manager.should_receive(:process_components)
+		@manager.javascripts
+  end
+
+ 	it "should not call process_components if @javascripts exists" do
+		@manager.instance_variable_set(:@javascripts, @javascripts) 
+		@manager.should_not_receive(:process_components)
+		@manager.javascripts
+  end
+
+	it "should return @javascripts" do
+		@manager.instance_variable_set(:@javascripts, @javascripts)
+		@manager.javascripts.should == @javascripts
+	end
+end
+
+
+describe Yui4Rails::AssetManager, ".process_components" do
+	
+	before(:each) do
+	  @manager = Yui4Rails::AssetManager.new
+		@manager.instance_variable_get(:@stylesheets).should be_nil
+		@manager.instance_variable_get(:@javascripts).should be_nil
+	end
+	
+	it "should not duplicate resources" do
+		@manager.instance_variable_set(:@components, [:reset, :reset, :reset])
+		@manager.send(:process_components)
+		@stylesheets = @manager.instance_variable_get(:@stylesheets)
+		@stylesheets.should == ["reset/reset-min"]
+	end
+	
+  it "should add stylesheet resources for :reset" do
+		@manager.instance_variable_set(:@components, [:reset])
+		@manager.send(:process_components)
+		@manager.instance_variable_get(:@stylesheets).should include("reset/reset-min")
+  end
+
+  it "should add stylesheet resources for :fonts" do
+		@manager.instance_variable_set(:@components, [:fonts])
+		@manager.send(:process_components)
+		@manager.instance_variable_get(:@stylesheets).should include("fonts/fonts-min")
+  end
+
+	it "should use add_container_includes for :container" do
+		@manager.instance_variable_set(:@components, [:container])
+		@manager.should_receive(:add_container_includes)
+		@manager.send(:process_components)
+  end
+
+	it "should use add_container_includes for :datatable" do
+		@manager.instance_variable_set(:@components, [:datatable])
+		@manager.should_receive(:add_datatable_includes)
+		@manager.send(:process_components)
+  end
+
+	it "should use add_container_includes for :charts" do
+		@manager.instance_variable_set(:@components, [:charts])
+		@manager.should_receive(:add_charts_includes)
+		@manager.send(:process_components)
+  end
+
+	it "should use add_container_includes for :carousel" do
+		@manager.instance_variable_set(:@components, [:carousel])
+		@manager.should_receive(:add_carousel_includes)
+		@manager.send(:process_components)
+  end
+end
+
